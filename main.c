827ea66e5396f001dcab3bcff60feac109935a4e
olegtuzhilkin <ibx@inbox.ru>
2022-06-01 22:27:17 +0300

    Способ открытия устройства - libusb_open_device_with_vid_pid. Выводится информация о производителе, серийный номер и наименование устройства

--- main.c
+++ main.c
@@ -8,49 +8,62 @@ int main()
 {
     int r; // return value
 
-    printf("Hello World!\n");
+    printf("\n\nhid driver reader is starting\n\n");
 
     r = libusb_init(NULL);
-    if (r < 0)
+    if (r < 0){
         printf("ERROR: fail to init libusb code=%d\n", r);
-
-    libusb_set_debug(NULL, 3);
-
-    libusb_device **list;
-    ssize_t cnt = libusb_get_device_list(NULL, &list);
-    if (cnt < 0)
-        printf("ERROR: failed to get device list code=%zd\n", cnt);
-
-    for(ssize_t i = 0; i < cnt; i++){
-        libusb_device *dev = list[i];
-        struct libusb_device_descriptor desc;
-        int r = libusb_get_device_descriptor(dev, &desc);
-        if (r < 0)
-            printf("ERROR: failed to get device descriptor code=%d\n", r);
-
-        printf("VID=0x%X,\t PID=0x%X\n", desc.idVendor, desc.idProduct);
-        if (desc.idVendor == VID){
-
-            if (desc.idProduct == PID){
-                libusb_device_handle *handle;
-                r = libusb_open(dev, &handle);
-                if (r < 0){
-                    printf("ERROR: failed to open device code=%d\n", r);
-                    exit(r);
-                    //LIBUSB_ERROR_NO_MEM
-                }
-
-                unsigned char buf[100];
-                r = libusb_get_string_descriptor_ascii(handle, desc.iSerialNumber, buf, sizeof (buf));
-                if (r < 0){
-                    printf("ERROR: failed to get serial number code=%d\n", r);
-                    exit(r);
-                }
-
-                printf("\n\n%s\n\n", buf);
-            }
-        }
+        exit(r);
     }
+
+    libusb_set_debug(NULL, LIBUSB_LOG_LEVEL_INFO);
+
+    libusb_device_handle *handle = libusb_open_device_with_vid_pid(NULL, VID, PID);
+    if (handle == NULL){
+        printf("ERROR: failed to open device VID=0x%x, PID=0x%x\n", VID, PID);
+        exit(0);
+    }
+
+    libusb_device *dev = libusb_get_device(handle);
+    if (dev == NULL){
+        printf("ERROR: failed to get device\n");
+        exit(0);
+    }
+
+    struct libusb_device_descriptor desc;
+    r = libusb_get_device_descriptor(dev, &desc);
+    if (r < 0){
+        printf("ERROR: failed to get device descriptor code=%d\n", r);
+        exit(r);
+    }
+
+
+
+    printf("\nDevice VID: 0x%X; PID=0x%X\n\n", VID, PID);
+    unsigned char buf[100];
+    r = libusb_get_string_descriptor_ascii(handle, desc.iProduct, buf, sizeof (buf));
+    if (r < 0){
+        printf("ERROR: failed to get product code=%d\n", r);
+        exit(r);
+    }
+    printf("Product: %s\n", buf);
+
+    r = libusb_get_string_descriptor_ascii(handle, desc.iManufacturer, buf, sizeof (buf));
+    if (r < 0){
+        printf("ERROR: failed to get manufacturer code=%d\n", r);
+        exit(r);
+    }
+    printf("Manufacturer: %s\n", buf);
+
+    r = libusb_get_string_descriptor_ascii(handle, desc.iSerialNumber, buf, sizeof (buf));
+    if (r < 0){
+        printf("ERROR: failed to get serial number code=%d\n", r);
+        exit(r);
+    }
+    printf("Serial number: %s\n", buf);
+
+
+    libusb_close(handle);
 
     return 0;
 }
